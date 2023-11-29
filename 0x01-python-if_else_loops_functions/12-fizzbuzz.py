e3678342d673f081b32c51ea4400fc2d2e661e77
tagwa015 <tagwahassan15@gmail.com>
2023-11-28 18:15:28 -0800

    update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,9 +4,8 @@ def fizzbuzz():
         if i % 15 == 0:
             print("FizzBuzz", end=" ")
         elif i % 5 == 0:
-            print("Buzz",end=" ")
-        elif
-        i % 3 == 0:
-            print("Fizz",end=" ")
+            print("Buzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
         else:
             print("{:d}".format(i), end=" ")
