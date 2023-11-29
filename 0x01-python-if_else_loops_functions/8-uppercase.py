23d4a247062da8381d47f372ce2a1c5c06fa8bc7
tagwa015 <tagwahassan15@gmail.com>
2023-11-28 16:00:37 -0800

    upload

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-def uppercase(str):
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord(z):
+        return True
+    els:
+        return False
 
+    def uppercase(str):
+        for c in str:
+            print("{:c}".format(ord(c) if not islower(c) else ord(c) - 32),end="")
+            print("")
