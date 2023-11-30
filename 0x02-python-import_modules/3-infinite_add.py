91bd9940dd47cfb66f793fba8a07ad0d4a297180
tagwa015 <tagwahassan15@gmail.com>
2023-11-30 15:31:18 -0800

    upload

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
-    total_sum = 0
-    for i in range(1, len(sys.argv)):
-        total_sum += int(sys.argv[i])
-        print(total_sum)
+    """
+    Print the addition of all arguments.
+    """
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
