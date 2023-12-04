8d9d5c0deca4d6f2bb7a62a33d42c416740b57bf
tagwa015 <tagwahassan15@gmail.com>
2023-12-03 17:07:08 -0800

    update

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{:d}".format(num))
+
+
+# Example usage
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
