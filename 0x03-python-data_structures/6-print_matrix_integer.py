06ac2c233ad4f727f59cb9eaca38bb6d30669901
tagwa015 <tagwahassan15@gmail.com>
2023-12-05 11:42:35 -0800

    edit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,4 +7,5 @@ def print_matrix_integer(matrix=[[]]):
         if len(submatrix) == 0:
             print()
         for i in range(len(submatrix)):
-            print("{:d}".format(submatrix[i]), end="\n" if i == len(submatrix) - 1 else " ")
+            print("{:d}".format(submatrix[i]),
+                  end="\n" if i == len(submatrix) - 1 else " ")
